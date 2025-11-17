51244120b2e554fe8921eec3f5445b4e72209c91
LukeSwier <lswier@unomaha.edu>
2025-11-17 04:15:47 +0000

    Finished Lab

--- main.py
+++ main.py
@@ -2,3 +2,20 @@
 #Name:
 #Date:
 #Assignment:
+
+import cars
+import pandas
+import matplotlib.pyplot as plt
+years = []
+CityMPGs = []
+cars = cars.get_car()
+for gear in cars:
+    year = gear["Identification"]["Year"]
+    CityMPG = gear["Fuel Information"]["City mpg"]
+    years.append(year)
+    CityMPGs.append(CityMPG)
+
+df = pandas.DataFrame([{"Year": years, "City MPG": CityMPGs}])
+
+plt.plot(years, CityMPGs)
+plt.savefig("DataTable")
